1cb436627537833efabf9241e7f8021582e371bf
AnHyeongryeol <reversiblehope@gmail.com>
2024-05-08 17:57:47 +0900

    질문목록 상세 페이지

---
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.DataNotFoundException;
+import java.util.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+  private final QuestionRepository questionRepository;
+
+  public List<Question> getList() {
+    return this.questionRepository.findAll();
+  }
+
+  public Question getQuestion(Integer id) {
+    Optional<Question> question = this.questionRepository.findById(id);
+    if (question.isPresent()) {
+      return question.get();
+    } else {
+      throw new DataNotFoundException("question not found");
+    }
+  }
+}
